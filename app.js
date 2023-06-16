3b46a2f23065771086d65fef0bcbbd409a0bea74
joshua <iuttn123@gmail.com>
2023-06-16 17:03:57 +0900

    ..

--- app.js
+++ app.js
@@ -1,15 +1,101 @@
 const express = require("express");
 const app = express();
-const { connectToDatabase } = require("./database");
-const { setupMiddleware } = require("./middleware");
-const { setupLoginRoutes } = require("./routes/loginRoutes");
-const { setupCheckAuthenticationRoutes } = require("./routes/checkAuthenticationRoutes");
+const { connectToDatabase, loginAdmin } = require("./database");
 const { port } = require("./configs");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const { Strategy: LocalStrategy } = require("passport-local");
 
 connectToDatabase(); // 데이터베이스 연결
-setupMiddleware(app); // 미들웨어 설정
-setupLoginRoutes(app); // 로그인 라우트 설정
-setupCheckAuthenticationRoutes(app);
+
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "09girl",
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 600 * 1000, // 5분(밀리초 단위로 지정)
+    },
+  })
+);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // 허용할 도메인을 설정합니다.
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+app.use(passport.initialize({})); // Initialize Passport middleware
+app.use(
+  passport.session({
+    secret: "09girl",
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 600 * 1000, // 5분(밀리초 단위로 지정)
+    },
+  })
+);
+app.use(bodyParser.json());
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+      session: true,
+      passReqToCallback: false,
+    },
+    async (username, password, done) => {
+      try {
+        const result = await loginAdmin(username, password);
+        console.log(result);
+        if (result) {
+          return done(null, username);
+        } else {
+          return done(null, false, { message: "로그인 실패" });
+        }
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+// Passport session setup.
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((username, done) => {
+  // 데이터베이스에서 사용자를 조회한 후, 유효한 사용자인 경우에는 done(null, user)로 사용자 객체를 전달해야 합니다.
+  // 사용자를 찾을 수 없는 경우에는 done(null, null)을 호출합니다.
+  done(null, null); // 데이터베이스 조회 로직을 추가해야 합니다.
+});
+
+app.post("/login", passport.authenticate("local"), (req, res) => {
+  req.login(req.user, (err) => {
+    if (err) {
+      return res.status(500).json({ success: false, message: "로그인 실패" });
+    }
+    return res.json({ success: true, message: "로그인 성공", data: req.user });
+  });
+});
+
+app.get("/checkAuthentication", async (req, res) => {
+  console.log("req.isAuthenticated()", req.isAuthenticated());
+  if (req.isAuthenticated()) {
+    res.status(200).json({ isAuthenticated: true, user: req.session.user });
+  } else {
+    res.status(200).json({ isAuthenticated: false, req: req.isAuthenticated });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- server.js
+++ server.js
@@ -1,13 +1,96 @@
 const express = require("express");
-const app = express();
+const cookieParser = require("cookie-parser");
+const { port } = require("./configs");
 
-// 데이터베이스 선택
-// const db = client.db("ToDoApp");
-// const collection = db.collection("post");
-// // 데이터 삽입
-// const items = {
-//   item1: { name: "홈런볼", price: 3000 },
-//   item2: { name: "허니버터칩", price: 2500 },
-//   item3: { name: "크림빵", price: 2000 },
-// };
-// const result = collection.insertOne(items);
+const app = express();
+app.use(cookieParser());
+
+app.get("/set-cookie", (req, res) => {
+  res.cookie("myCookie", "Hello, World!");
+  res.send("Cookie has been set.");
+});
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // 허용할 도메인을 설정합니다.
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
+
+app.get("/read-cookie", (req, res) => {
+  const myCookie = req.cookies.myCookie;
+  res.send(`The value of myCookie is: ${myCookie}`);
+});
+
+app.get("/greet", (req, res) => {
+  const name = req.cookies.name || "Guest";
+  res.send(`Hello, ${name}!`);
+});
+
+app.get("/login", (req, res) => {
+  // 세션 쿠키
+  res.cookie("sessionCookie", "abc123", { httpOnly: true });
+
+  // 영구 쿠키 (7일 동안 유지)
+  res.cookie("permanentCookie", "123abc", { maxAge: 7 * 24 * 60 * 60 * 1000 });
+
+  res.send("Logged in successfully.");
+});
+
+app.get("/logout", (req, res) => {
+  // 세션 쿠키 삭제
+  res.clearCookie("sessionCookie");
+
+  // 영구 쿠키 삭제
+  res.clearCookie("permanentCookie");
+
+  res.send("Logged out successfully.");
+});
+
+app.get("/secure-cookie", (req, res) => {
+  res.cookie("secureCookie", "123", { secure: true });
+  res.send("Secure cookie has been set.");
+});
+
+app.get("/http-only-cookie", (req, res) => {
+  res.cookie("httpOnlyCookie", "456", { httpOnly: true });
+  res.send("HttpOnly cookie has been set.");
+});
+
+app.get("/path-cookie", (req, res) => {
+  res.cookie("pathCookie", "789", { path: "/specific-path" });
+  res.send("Path cookie has been set.");
+});
+
+app.get("/domain-cookie", (req, res) => {
+  res.cookie("domainCookie", "xyz", { domain: ".example.com" });
+  res.send("Domain cookie has been set.");
+});
+
+// 사용자 로그인
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  // 사용자 인증 로직
+  if (isValidUser(username, password)) {
+    // 인증 성공 시 쿠키에 사용자 정보 저장
+    res.cookie("loggedInUser", username);
+    res.send("Logged in successfully.");
+  } else {
+    res.status(401).send("Invalid credentials.");
+  }
+});
+
+// 인증된 사용자만 접근 가능한 페이지
+app.get("/secret", (req, res) => {
+  const loggedInUser = req.cookies.loggedInUser;
+  if (loggedInUser) {
+    res.send(`Welcome, ${loggedInUser}! This is a secret page.`);
+  } else {
+    res.status(401).send("Unauthorized access.");
+  }
+});
+
+app.listen(8080, () => {
+  console.log(`Server is running on 8080 ${port}`);
+});
