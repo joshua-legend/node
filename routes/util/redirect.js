c64dc4afd59f3894cb92e0e14f8588d281424b30
joshua <iuttn123@gmail.com>
2023-07-25 11:11:06 +0900

    ..

--- routes/util/redirect.js
+++ routes/util/redirect.js
@@ -1,5 +1,6 @@
 const redirect = (req, res) => {
   const redirectUrl = req.query.state;
+  console.log(res.cookies);
   res.redirect(`http://localhost:3001${redirectUrl}`);
 };
 module.exports = redirect;
