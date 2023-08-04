a9bcc6bdb84408a57aab09731413ea1063c63f89
joshua <iuttn123@gmail.com>
2023-08-04 20:00:24 +0900

    ..

--- middlewares.js
+++ middlewares.js
@@ -8,10 +8,15 @@ exports.applyMiddlewares = (app) => {
   app.use(bodyParser.json());
   app.use(cookieParser()); // 이 부분을 추가해주세요.
   app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // 허용할 도메인을 설정합니다.
+    const origin = req.headers.origin;
+    const allowedOrigins = ["http://localhost:3001", "https://09-girl.vercel.app/"];
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
+    }
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Credentials", true);
+
     next();
   });
   app.use(passport.initialize());
